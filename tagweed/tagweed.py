7aa67ced06d8d1228765c422ad6bae28ee0282bc
Richard Shea <rshea@thecubagroup.com>
2014-04-11 23:14:01 +1200

    Chucked out docopt and replace with optparse. Amended help text to reflect tagweed

--- tagweed/tagweed.py
+++ tagweed/tagweed.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''tagweed2
+
+'''tagweed
 
 Usage:
-  tagweed2 ship new <name>...
-  tagweed2 ship <name> move <x> <y> [--speed=<kn>]
-  tagweed2 ship shoot <x> <y>
-  tagweed2 mine (set|remove) <x> <y> [--moored|--drifting]
-  tagweed2 -h | --help
-  tagweed2 --version
+  tagweed ship new <name>...
+  tagweed ship <name> move <x> <y> [--speed=<kn>]
+  tagweed ship shoot <x> <y>
+  tagweed mine (set|remove) <x> <y> [--moored|--drifting]
+  tagweed -h | --help
+  tagweed --version
 
 Options:
   -h --help     Show this screen.
@@ -18,11 +19,49 @@ Options:
   --drifting    Drifting mine.
 '''
 from __future__ import unicode_literals, print_function
-from docopt import docopt
+from optparse import OptionParser
+import os
+
+def parse_args():
+    '''
+    Parses command line arguments using OptionParser.
+    Applies validation rules to arguments and then, if OK
+    returns them in a 'dictionary like' object ``options``
+
+    '''
+    desc = "%prog is used to highlight tags which might be changed and so \n" + \
+    "improve the usefulness of the set of tags.\n" + \
+    "\n\n" + \
+    "Command line options specify the location of a config file and the" + \
+    "location of the tags."
+
+    usage_inner = "Usage: %s [options]"
+    usage = usage_inner % "%prog"
+
+
+    parser = OptionParser(description=desc, usage=usage)
+    parser.add_option(  "-c", "--config", action="store",  dest="cfg",
+                        metavar="CONFIG", help="Full path to config data")
+    parser.add_option(  "-t", "--tagsource", action="store", dest="tagsource",
+                        metavar="TAGSOURCE", help="Name of TAGSOURCE (currently only PINBOARD is supported")
+    parser.add_option(  "-v", "--verbose", action="store_true",
+                        dest="verbose", help="Show progress messages")
+
+    (options, args) = parser.parse_args() # pylint: disable=W0612
+
+
+    if (options.cfg is None) or (options.tagsource is None):
+        parser.print_help()
+        exit(-1)
+    elif not os.path.exists(options.cfg):
+        parser.error('config location does not exist')
+
+    return options
+
 
 def main():
-    '''Main entry point for the tagweed2 CLI.'''
-    args = docopt(__doc__, version=__version__)
+    '''Main entry point for the tagweed CLI.'''
+    args = parse_args()
     print(args)
 
 if __name__ == '__main__':
